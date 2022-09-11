bf503208a96503570a44fadd8370c584a126b218
AlexRequelme <alexdeveloper09@gmail.com>
2022-09-11 11:54:21 -0500

    Added mobile and web design for search films page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,39 @@ import { Route, Routes } from "react-router-dom";
 import DetailsFilmsPage from "./routes/DetailsFilmsPage";
 import ListFilmsPage from "./routes/ListFilmsPage";
 import SearchFilmsPage from "./routes/SearchFilmsPage";
+import { ReactComponent as GithubIcon } from "./assets/github.svg";
+import { ReactComponent as LinkedinIcon } from "./assets/linkedin.svg";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<SearchFilmsPage />} />
-      <Route path="expenses" element={<ListFilmsPage />} />
-      <Route path="invoices" element={<DetailsFilmsPage />} />
-    </Routes>
+    <>
+      <div className="flex min-h-[calc(100vh-3.5rem)] justify-center">
+        <Routes>
+          <Route path="/" element={<SearchFilmsPage />} />
+          <Route path="expenses" element={<ListFilmsPage />} />
+          <Route path="invoices" element={<DetailsFilmsPage />} />
+        </Routes>
+      </div>
+      <footer className="flex h-14 items-center justify-between bg-[#f6f6f6] px-4 text-gray-700 md:px-8">
+        © Alexander Requelme 2021
+        <div className="inline-flex gap-4">
+          <a
+            href="https://github.com/AlexRequelme"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GithubIcon className="h-5 w-5 fill-current" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/alexrequelme/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LinkedinIcon className="h-5 w-5 fill-current" />
+          </a>
+        </div>
+      </footer>
+    </>
   );
 }
 
--- client/src/routes/SearchFilmsPage.tsx
+++ client/src/routes/SearchFilmsPage.tsx
@@ -1,3 +1,22 @@
+import logo from "../assets/studio-ghibli-logo.png";
+import { ReactComponent as SearchIcon } from "../assets/search.svg";
+
 export default function SearchFilmsPage() {
-  return <div>Works!!!</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="Studio Ghibli Logo" width="300px" />
+      <div className="relative h-[46px] min-w-[320px] rounded-3xl border-2 border-gray-200 px-3 focus-within:border-blue-600 md:w-[584px]">
+        <div className="pointer-events-none absolute top-1/2 -translate-y-1/2 transform text-gray-400">
+          <SearchIcon className="h-5 w-5" />
+        </div>
+        <input
+          type="text"
+          className="h-full w-full appearance-none bg-transparent pl-8 text-base outline-none"
+        />
+      </div>
+      <button className="rounded bg-gray-500 px-8 py-2 text-white hover:bg-gray-600">
+        Search
+      </button>
+    </div>
+  );
 }
